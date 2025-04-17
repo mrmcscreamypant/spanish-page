ed05a761c827b44b2cacc4f09d5b7906cc015424
mrmcscreamypant <161328176+mrmcscreamypant@users.noreply.github.com>
2025-04-17 15:06:33 +0000

    A

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import Page from './pages/page';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Page></Page>
-      </header>
+      <Page></Page>
     </div>
   );
 }
--- src/pages/page.jsx
+++ src/pages/page.jsx
@@ -1,5 +1,26 @@
-function Page(){
-    return (<p>It works</p>)
+function Section({children,name}){
+    return (
+        <div className="area">
+            <h2>{name}</h2>
+            <p>{children}</p>
+        </div>
+    )
 }
+
+function Page(){
+    return (
+        <>
+            <Section name="Mi Barrio">Se llama Chestnut Hill<br/>Esta en Brookline</Section>
+            <Section name="Es...">Tranquilo y Grande</Section>
+            <Section name="No Es...">Turistico o Ruidoso</Section>
+            <Section name="Hay En Mi Barrio">Hay el tren, el supermercado, y mi escuela</Section>
+            <Section name="No Hay...">un aeropuerto, un volcán, o un isla</Section>
+            <Section name="Yo va a">En la ano de escuela yo voy a mi escuela</Section>
+            <Section name="Me gusta">Es Tranquilo</Section>
+            <Section name="No me gusta">Es Grande</Section>
+        </>
+    )
+}
+
 
 export default Page;
